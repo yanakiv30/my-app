ffce51712a6a8a27a5a2b7264e8f66130f31ad96
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-01-26 14:46:50 +0200

    reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useReducer } from 'react';
+
 export default function App() {
-return <div>
-  hello
-</div>
+  
+  function reducer(state, action) {
+return state +action;
+}
+  
 
+  // const [sum, dispatch] = useReducer((state, action) => {
+  //   return state + action;
+  // }, 0);
 
+  const [sum, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <p>
+      <span style={{fontSize:"44px"}}>{sum} &ensp;</span>
+      <button onClick={() => dispatch(1)} style={{fontSize:"44px"}} >
+      Add 1
+      </button>
+    </p>
+  );
 }
